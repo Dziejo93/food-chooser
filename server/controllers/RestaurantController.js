339e45a0b56ab327ba730993fe20e5bb0c1476f4
Andrzej Gatkowski <gatkowski.a@gmail.com>
2018-09-03 00:31:33 +0200

    fixed deleted Restaurant model in RestaurantController

--- server/controllers/RestaurantController.js
+++ server/controllers/RestaurantController.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
-
+const Restaurant = require("../models/restaurant-model")
 
 module.exports = {
 	getRestaurants: async (req, res, next) => {

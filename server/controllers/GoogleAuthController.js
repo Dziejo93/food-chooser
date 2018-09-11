7c9d79c160ffa20bd7eced6cf503545801527186
Andrzej Gatkowski <gatkowski.a@gmail.com>
2018-09-11 23:46:59 +0200

    first instance of full working get on google plus api

--- server/controllers/GoogleAuthController.js
+++ server/controllers/GoogleAuthController.js
@@ -5,8 +5,8 @@ const GoogleApi = require("../helpers/googleApi")
 const {
 	google
 } = require("googleapis")
+const plus = google.plus("v1")
 const OAuth2 = google.auth.OAuth2
-
 const oauth2Client = new OAuth2(
 	process.env.GOOGLE_CLIENT_ID,
 	process.env.GOOGLE_CLIENT_SECRET,
@@ -15,6 +15,7 @@ const oauth2Client = new OAuth2(
 
 const scopes = ["https://www.googleapis.com/auth/plus.me",
 	"https://www.googleapis.com/auth/userinfo.email",
+
 ]
 
 const url = oauth2Client.generateAuthUrl({
@@ -51,21 +52,45 @@ module.exports = {
 		const code = req.body.googleCode
 		console.log(code)
 
-		oauth2Client.getToken(code, function (err, tokens) {
+		oauth2Client.getToken(code, async (err, tokens) => {
 			if (err) {
-				console.log(err)
-				res.status(404).send({message:err})
+				console.log("dupa", err)
+				res.status(404).send({
+					message: "err"
+				})
+			} else {
+
+				oauth2Client.setCredentials(tokens)
+
+				plus.people.get({userId:"me",auth:oauth2Client},async(err,response)=>{
+					if(err){console.log(err)
+					}else{
+						console.log(response)
+
+					}
+				})
+				console.log(tokens.access_token)
+
+
+				res.status(200).send({
+					tokens
+				})
+				//  oauth2Client.setCredentials({access_token:tokens.access_token})
+
+				// const me = await plus.people.get({ userId: "me" })
+				// console.log(me)
+
+				// const userGoogleId = me.data.id
+				// const userGoogleEmail = me.data.emails && me.data.emails.length && me.data.emails[0].value
+
+				// res.status(200).send({
+				// 	id:userGoogleId,
+				// 	gmail:userGoogleEmail,
+				// 	tokens:tokens})}
+
 
 			}
-
-			console.log(tokens)
-			oauth2Client.setCredentials(tokens)
-
-			res.status(200).send({message:tokens})
-
 		})
-
-
 	}
 
 
